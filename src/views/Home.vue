223d64073b7c0ccab1bf494d4e7a1a5f34e97cc9
pauffret <pierreauffret@free.fr>
2020-11-18 09:27:10 +0100

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="home">
+    <div class="title">
+      <h1>Bienvenue sur WorkshopDashboard !</h1>
+    </div>
+      <b-container class="bv-example-row">
+        <b-row>
+          <b-col>
+            <b-card title="nb personnes dans le batiment" sub-title="Card subtitle">
+              <b-card-text>
+                Some quick example text to build on the <em>card title</em> and make up the bulk of the card's
+                content.
+              </b-card-text>
+
+              <b-card-text>A second paragraph of text in the card.</b-card-text>
+            </b-card>
+          </b-col>
+          <b-col>
+            <b-card title="nb d'entrée" sub-title="Card subtitle">
+              <b-card-text>
+                Some quick example text to build on the <em>card title</em> and make up the bulk of the card's
+                content.
+              </b-card-text>
+
+              <b-card-text>A second paragraph of text in the card.</b-card-text>
+            </b-card>
+          </b-col>
+          <b-col>
+            <b-card title="nb sortie" sub-title="Card subtitle">
+              <b-card-text>
+                Some quick example text to build on the <em>card title</em> and make up the bulk of the card's
+                content.
+              </b-card-text>
+
+              <b-card-text>A second paragraph of text in the card.</b-card-text>
+            </b-card>
+          </b-col>
+        </b-row>
+
+        <b-row>
+          <b-col>
+            <b-card title="nb entrée de la semaine" sub-title="Card subtitle">
+              <b-card-text>
+                Some quick example text to build on the <em>card title</em> and make up the bulk of the card's
+                content.
+              </b-card-text>
+
+              <b-card-text>A second paragraph of text in the card.</b-card-text>
+            </b-card>
+          </b-col>
+          <b-col>
+            <b-card title="nb entrée du mois" sub-title="Card subtitle">
+              <b-card-text>
+                Some quick example text to build on the <em>card title</em> and make up the bulk of the card's
+                content.
+              </b-card-text>
+
+              <b-card-text>A second paragraph of text in the card.</b-card-text>
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-container>d
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
